af0fabe6784a46499a905162222aee562a9eecdb
Damith Senadeera <damith1292@gmail.com>
2023-05-26 15:59:37 +0100

    test 5

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,7 @@ int main(int argc, char *  argv[]) {
 	int            numSampA, numSampB;
 	double         A, B, C;
 
-	//std::istringstream iss;
+	std::istringstream iss;
 
 	std::string line ;
 	
@@ -22,6 +22,60 @@ int main(int argc, char *  argv[]) {
 	
 	std::getline(aFile, line);
 	std::cout << "line" << std::endl ;
+
+	iss.str(line) ;
+	iss >> numSampA;
+	
+	
+	samplesA = (double *) calloc(numSampA, sizeof(double));
+	
+	for (int i = 0 ; i < numSampA; i++) {
+		
+		//std::getline(aFile, &samplesA[i]);
+		std::getline(aFile, line);
+		iss.clear();
+		iss.str(line) ;
+		iss >> &samplesA[i];
+		
+	}
+	
+	aFile.close();
+	
+	
+	bFile.open("input-B.txt");
+	
+	//std::getline(bFile, numSampB);
+	std::getline(bFile, line);
+	iss.str(line) ;
+	iss.clear();
+	iss >> numSampB ;
+	
+	samplesB = (double *) calloc(numSampB, sizeof(double));
+	
+	for (int i = 0 ; i < numSampB; i++) {
+		
+		//std::getline(bFile, &samplesB[i]);
+		std::getline(bFile, line);
+		iss.str(line) ;
+		iss.clear();
+		iss >> &samplesB[i];
+		
+	}
+	
+	bFile.close();
+	
+	A = libUncertainDoubleDistFromSamples(samplesA, numSampA);
+	
+	B = libUncertainDoubleDistFromSamples(samplesB, numSampB);
+	
+	C = A + B ;
+	
+	std::cout << "Implicit calculation :" << std::endl ;
+	
+	std::cout << "A = " << A << std::endl ;
+	std::cout << "B = " << B << std::endl ;
+	
+	std::cout << "C = A + B = " << C << std::endl ;
 	
 	
 	
